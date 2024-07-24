0a31b1536d37d7b95a849724da91fc6f7c0d67f9
tenergase <justinqdinh@yahoo.com>
2024-07-24 03:54:35 -0400

    Rename Date_Saved column back to Date Change console message for save success Add comments for todo

--- Scripts/Save.R
+++ Scripts/Save.R
@@ -7,13 +7,16 @@ Save.All <- function(silent = FALSE) {
 
 ### Save.History
 # Appends the output of Update.All() to centerHistory.rds
+# WORKS ONLY FOR CURRENT DATE - NEED TO PASS DATE ARGUMENT INTO UPDATE.ALL()
+# MAYBE CHECK IF STRUCTURE DIFFERENT THEN CALL SAVE.ALL()
 Save.History <- function(date = Sys.Date(), ignoreMissing = F, silent = F) {
   fileName <- "Cache/centerHistory.rds"
   filePath <- file.path(getwd(), fileName)
   
-  history <- mutate(Update.All(get=TRUE), Date_Saved = Sys.Date(), .before = "Account_Id")
+  history <- mutate(Update.All(get=TRUE), Date = date, .before = "Account_Id")
   
   # Remove [ and ] from column names
+  # PUT THIS IN AN UPDATE FUNCTION
   names(history) <- gsub("[][]", "", names(history))
   
   if(file.exists(filePath)) {
@@ -25,5 +28,5 @@ Save.History <- function(date = Sys.Date(), ignoreMissing = F, silent = F) {
   
   saveRDS(dat, filePath)
   
-  if(!silent) print("Student data saved!")
+  if(!silent) cat("Notice: Center history saved for ", as.character(date), "!", sep="")
 }
