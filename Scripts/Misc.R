38b04f53a3a8d498323d3ac1f84442b86209b672
tenergase <justinqdinh@yahoo.com>
2024-07-26 14:58:53 -0400

    rename function names in Misc.R

--- Scripts/Misc.R
+++ Scripts/Misc.R
@@ -1,7 +1,7 @@
-### getRawDataEmptyCols
+### get_raw_na_cols
 # Returns a named list of vectors for names of columns containing only NAs in raw data files
 # MODIFY TO ALLOW ITERATION THROUGH MULTIPLE DATES?
-getRawDataEmptyCols <- function(date = today()) {
+get_raw_na_cols <- function(date = today()) {
   fileRoots <- c("Students Export  ", 
                  "Account Export  ", 
                  "Current Batch Detail Export  ", 
@@ -22,11 +22,11 @@ getRawDataEmptyCols <- function(date = today()) {
   return(na_col_list)
 }
 
-### printRawDataEmptyCols
+### print_raw_na_cols
 # Prints names of NA columns in raw data files
 # Formatted for copy/paste into a vector in code
-printRawDataEmptyCols <- function(date = today()) {
-  na_col_list = getRawDataEmptyCols(date = date)
+print_raw_na_cols <- function(date = today()) {
+  na_col_list = get_raw_na_cols(date = date)
   
   for (col_name in names(na_col_list)) {
     cat(col_name, ": \"", sep="")
