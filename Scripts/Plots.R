9d6fa84a2f0a155535688c42abc0676ab025bcf7
PhiPrime <lrc0048@hawkmail.hacc.edu>
2024-08-02 14:20:56 -0400

    Added an outlier tester/remover to the correlation plot function. Knitted the pdf for 8-2-2024

--- Scripts/Plots.R
+++ Scripts/Plots.R
@@ -1,25 +1,57 @@
 ####################     GRAPH GENERATING FUNCTIONS     #####################
 
 ####################     EXPLORATORY PLOT FUNCTIONS     #####################
-plotHighestCor <- function(dataset, flip = F){
+plotHighestCor <- function(dataset, removeOutliers = FALSE, oCI = 99){
+  #Filter out col with numeric datatypes, any uncertainty is made 0 by default
   numdata <- dplyr::select_if(dataset, is.numeric)
   numdata[is.na(numdata)] <- 0
-  if(any(toupper(names(numdata))=="PEST")){#quick fix, should use apply
-    numdata$Pest[is.infinite(numdata$Pest)] <- 0
-  }
+  numdata[sapply(numdata, is.infinite)] <- 0
   
+  
+  
+  
+  #Find corelations that aren't (nearly) direct and assign value of 0
   cor_mat <- cor(numdata)
   cor_mat[cor_mat >= .98] <- 0
   
+  #Find which variables have the largest correlation
   max_cor_inds <- which(cor_mat == max(abs(cor_mat)), arr.ind = T)
   max_row <- rownames(cor_mat)[max_cor_inds[1,1]]
   max_col <- colnames(cor_mat)[max_cor_inds[1,2]]
   
-  # par(mfrow = c(1,2))
-  p1 <- ggplot(numdata, aes(x=numdata[[max_row]], y=numdata[[max_col]])) + 
-    geom_point() + geom_smooth() + labs(x = max_row, y = max_col)
+  #If desired, use grubbs test to remove high and low outliers
+  if(removeOutliers){
+    alpha <- 1-oCI/100
+    #For both row and col...
+    for(vname in c(max_row,max_col)){
+      #For each's highest and lowest...
+      for(LOWEST in c(F,T)){
+        #While p isn't higher than User defined alpha
+        p <- 0
+        while(p<alpha){
+          p <- outliers::grubbs.test(numdata[[vname]], opposite = LOWEST)$p
+          if(p<alpha){
+            #remove observation from numdata
+            #Remove row with max value in numdata$vname; 
+            #using (-1)^1 and (-1)^0 to toggle highest/lowest
+            numdata <- numdata[-which.max(
+              (-1)^LOWEST*numdata[[vname]]),]
+        }
+      }#while
+    }#High/Low
+  }#for loop finished, outliers should be removed
+}#End of outlier section
   
-  p2 <- ggplot(numdata, aes(x=numdata[[max_col]], y=numdata[[max_row]])) + 
-    geom_point() + geom_smooth() + labs(x = max_col, y = max_row)
+  # Standard plot to produce
+  stdplot <- function(whatOnX, whatOnY){
+    ggplot(numdata, aes(x=.data[[whatOnX]], y=.data[[whatOnY]])) + 
+      geom_point() + 
+      geom_smooth(method = "loess", formula = y~x) + 
+      labs(x = whatOnX, y = whatOnY)
+  }
+  
+  # Make with swapped axes since independent variable is unknown
+  p1 <- stdplot(max_row,max_col)
+  p2 <- stdplot(max_col,max_row)
   p1+p2
 }
