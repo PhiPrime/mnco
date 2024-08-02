a4b97ec1fc5760d73cc64502d198815a6b9b9683
PhiPrime <lrc0048@hawkmail.hacc.edu>
2024-08-02 14:25:41 -0400

    Arbitrarily changed max correlation to 0.97

--- Scripts/Plots.R
+++ Scripts/Plots.R
@@ -12,7 +12,7 @@ plotHighestCor <- function(dataset, removeOutliers = FALSE, oCI = 99){
   
   #Find corelations that aren't (nearly) direct and assign value of 0
   cor_mat <- cor(numdata)
-  cor_mat[cor_mat >= .98] <- 0
+  cor_mat[cor_mat >= .97] <- 0
   
   #Find which variables have the largest correlation
   max_cor_inds <- which(cor_mat == max(abs(cor_mat)), arr.ind = T)
