c92ceb005723d40295047c0a2871ec4bfc03aca9
tenergase <justinqdinh@yahoo.com>
2024-08-03 17:23:49 -0400

    Add documentation for radiusFileRoots

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Radius file roots
+#'
+#' All Radius files that can be processed by mnco.
+#'
+#' @format A character vector of file roots:
+#' \describe{
+#'   \item{Students Export}{Student information}
+#'   \item{Account Export}{Account information}
+#'   \item{Current Batch Detail Export}{Trailing 30 days data on attendance
+#'     and skills mastered}
+#'   \item{Enrollment Report}{Enrollment information}
+#' }
+"radiusFileRoots"
