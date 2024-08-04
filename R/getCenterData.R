c4aa8e620ad08e718704192dcf564eb9a66b5d56
tenergase <justinqdinh@yahoo.com>
2024-08-04 13:33:49 -0400

    Continue work on getCenterData() and readRawData()

--- R/getCenterData.R
+++ R/getCenterData.R
@@ -9,33 +9,45 @@
 #' @export
 #'
 #' @examples
-getCenterData <- function(rawDataDir, data = "all",
+getCenterData <- function(dir, type = "all",
                           date = Sys.Date(), ignoreMissing = F) {
+  # Directory must exist
+  if (!dir.exists(dir)) stop(dir, " does not exist!")
 
-  for (fileRoot in radiusFileRoots) {
+  if (type == "all") {
+    unmerged <- list()
+
+    for (.data in names(radiusFileRoots)) {
 
 
-    unmerged <- unmerged %>% append(dat)
+      unmerged <- unmerged %>% append(dat)
+    }
+  } else if (type %in% names(radiusFileRoots)) {
+    rawFileName <- as.rawFileName(radiusFileRoots[[type]], date)
+    rawFilePath <- file.path(dir, rawFileName)
+
+
+
+
+    dat <- readRawData
+  } else {
+    stop(type, " is not a valid argument to 'data'.")
   }
 
-  # # Read and process raw data
-  # students <- getStudentData(date, ignoreMissing)
-  # accounts <- getAccountData(date, ignoreMissing)
-  # progress <- getProgressData(date, ignoreMissing)
-  # enrollments <- getEnrollmentData(date, ignoreMissing)
-  #
-  # # Merge into one data frame
-  # # NEED TO EXAMINE MERGING
-  # # MERGE getStudentRanking
-  # all <- mergeWithFill(students, accounts, .by = "Account_Id")
-  # all <- merge(all, progress, all.x = TRUE)
-  # all <- merge(all, enrollments, all.x = TRUE)
-
-  invisible(centerData)
+  invisible(tdat)
 }
 
-as.rawFileName <- function(fileRoot, date) {
-  paste0(fileRoot, "  ", as.radiusDate(date), ".xlsx")
+readRawData <- function(dir, data, date = Sys.Date)
+readRawData <- function(path) {
+  if (!file.exists(path)) {
+    stop(path, " does not exist!")
+  }
+}
+
+# as.rawFilePath
+
+as.rawFileName <- function(root, date) {
+  paste0(root, "  ", as.radiusDate(date), ".xlsx")
 }
 
 as.radiusDate <- function(date) {
