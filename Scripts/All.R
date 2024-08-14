c26535f98059c365b54b1e46597b61b08f123f59
tenergase <justinqdinh@yahoo.com>
2024-08-14 12:40:46 -0400

    Delete old files not needed for package

--- Scripts/All.R
+++
@@ -1,7 +0,0 @@
-#######################     SOURCE ALL SCRIPTS     #########################
-scripts <- list.files("./Scripts")
-scripts <- scripts[grepl(".R$", scripts)&
-                     !grepl("All.R$", scripts)]
-scripts <- paste0("Scripts/", scripts)
-sapply(scripts, source)
-rm(scripts)
