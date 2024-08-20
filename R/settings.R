001f69678a0cfa8535f7e34c74d2221ddd7c93a1
pmcrow <23pcrowe@gmail.com>
2024-08-20 07:31:24 -0400

    Overhauled the settings access. Currently stores settings values in a dataframe within globalenv(). .dat file stores them for persistence.

--- R/settings.R
+++ R/settings.R
@@ -1,4 +1,61 @@
+#Name of the settings file
 settings_file <- "centerOverviewSettings.dat"
+global <- globalenv()
+
+#Basis for assignment, not for use, will be deleted later
+base_settings <- data.frame(
+  Recent_Assessments_Date="",
+  Attendance_Allowed_Days=NA,
+  Deck_Minimum_Threshold=NA,
+  Deck_Warning_Duration=NA,
+  Deck_Suppression_Maximum_Time=NA,
+  Standard_Contract_Length=NA,
+  Standard_Number_Sessions=NA,
+  Price_Upper_Bound=NA,
+  Price_Lower_Bound=NA,
+  Student_Upper_Bound=NA,
+  Student_Lower_Bound=NA,
+  Length_Modifier=NA,
+  Contract_Adjustment_Lower_Bound=NA,
+  Contract_Adjustment_Upper_Bound=NA,
+  Contract_Length_Upper_Bound=NA,
+  Contract_Length_Lower_Bound=NA,
+  Raw_Data_Directory="",
+  Cache_Directory=""
+)
+
+#Create settings file if it exists upon the package being loaded
+.onLoad <- function(libname,pkgname) {
+  #create the settings file if none exists
+  if(!file.exists(settings_file))
+    initialize_settings_file()
+
+  #Assign global environment to settings
+  assign("global_settings",base_settings, envir = global)
+
+  #Establish connection and read the lines into 'text_output'
+  file_connection <- file(settings_file)
+  text_output  <- readLines(settings_file)
+
+  #read the value of each line into the data frame
+  for (i in 1:length(text_output)) {
+    #retrieve variable name
+    variable_name <- colnames(global$global_settings)[i]
+
+    #get match in the file
+    if (grepl(variable_name, text_output[i])) {
+      global$global_settings[i] <- gsub(paste0(variable_name,"="), "",text_output[i])
+    }
+  }
+
+  #Close connection and return the value
+  close(file_connection)
+
+  #print message
+  message("Settings successfully loaded")
+}
+
+
 
 #' Create Settings File
 #'
@@ -36,12 +93,14 @@ initialize_settings_file <- function() {
           "Contract_Length_Lower_Bound=1",
           "Raw_Data_Directory=UNINITIALIZED",
           "Cache_Directory=UNINITIALIZED"
-          ),
-        settings_file, append = FALSE)
+  ),
+  settings_file, append = FALSE)
 
   #close the connection
   close(file_connection)
 }
+
+
 
 #' Retrieve a variable from the settings file
 #'
@@ -52,31 +111,16 @@ initialize_settings_file <- function() {
 #' @export
 retrieve_variable <- function(variable_name, is_numeric = TRUE) {
 
-  #Create default if no settings file exists
-  if (!file.exists(get_file_name()))
-    initialize_settings_file()
-
-  #Adjust parameters
-  variable_name <- paste(variable_name, "=", sep="")
-  value <- NA
-
-  #Establish connection and read the lines
-  file_connection <- file(settings_file)
-  text_output  <- readLines(settings_file)
-
-  #Brute force to find vector and retrieve the value
-  for (i in 1:length(text_output)) {
-    if (grepl(variable_name, text_output[i])) {
-      value <- gsub(variable_name, "",text_output[i])
-    }
-  }
-
-  #Close connection and return the value
-  close(file_connection)
-  if (!is_numeric)
-    return(value)
+  #Brute force because of short list
+  #Should fix this later
+  for (i in 1:length(global$global_settings))
+    if (colnames(global$global_settings)[i] == variable_name)
+      if (is_numeric)
+        return(strtoi(global$global_settings[i]))
   else
-    return(strtoi(value))
+    return(global$global_settings[i])
+
+  message("Did not find the value")
 }
 
 #' Edit a variable in the settings file
@@ -86,6 +130,16 @@ retrieve_variable <- function(variable_name, is_numeric = TRUE) {
 #'
 #' @export
 edit_variable <- function(variable_name, value) {
+
+  #Force numeric values to be numeric
+  if (!is.na(strtoi(value)))
+    value <- strtoi(value)
+
+  #Assign 'value' to global_settings
+  #Brute force for short list
+  for (i in 1:length(global$global_settings))
+    if (colnames(global$global_settings)[i] == variable_name)
+      global$global_settings[i] <- value
 
   #Establish connection and read lines
   file_connection <- file(settings_file)
@@ -101,5 +155,27 @@ edit_variable <- function(variable_name, value) {
     }
   }
   #Close connection
+  close(file_connection)
+
+  #Will fix later but here to fix a bug in UI
+  refresh_settings()
+}
+
+refresh_settings <- function() {
+  file_connection <- file(settings_file)
+  text_output  <- readLines(settings_file)
+
+  #read the value of each line into the data frame
+  for (i in 1:length(text_output)) {
+    #retrieve variable name
+    variable_name <- colnames(global$global_settings)[i]
+
+    #get match in the file
+    if (grepl(variable_name, text_output[i])) {
+      global$global_settings[i] <- gsub(paste0(variable_name,"="), "",text_output[i])
+    }
+  }
+
+  #Close connection and return the value
   close(file_connection)
 }
