a4a8c5b6b3ae19b7c0cee86b31d9d23e5ead2ee8
tenergase <justinqdinh@yahoo.com>
2024-08-28 12:03:11 -0400

    Add aliases for getCenterData() for convenience in interactive testing ONLY

--- R/center-data-get.R
+++ R/center-data-get.R
@@ -156,3 +156,12 @@ getAttendanceData <- function(get = FALSE, date = Sys.Date()) {
 
   invisible(NULL)
 }
+
+# these functions are intended ONLY for convenience in interactive testing with
+#   load_all() (or ctrl+shift+L)
+# do NOT use these in a package function. use otherwise at risk of them being
+#   removed at any time
+cstu <- function() getCenterData("student")
+cacc <- function() getCenterData("account")
+cenr <- function() getCenterData("enrollment")
+cpro <- function() getCenterData("progress")
