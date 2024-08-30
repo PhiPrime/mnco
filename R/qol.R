9d76fce41e8a71678d7c983b09ed939b762acc38
tenergase <justinqdinh@yahoo.com>
2024-08-30 04:18:48 -0400

    Create dailyReport(), which knits daily-report.Rmd and opens daily-report.pdf from the mnco directory

--- R/qol.R
+++ R/qol.R
@@ -11,3 +11,24 @@ cpay <- function() getCenterData("payment")
 ccur <- function() getCenterData("curriculum")
 catt <- function() getCenterData("attendance")
 ctem <- function() getCenterData("template")
+
+
+#' Knits daily-report.Rmd and, by default, opens it
+#'
+#' Recommended workflow for testing changes to daily-report.pdf is:
+#'   install()/ctrl+shift+B
+#'   dailyReport()
+#' If using already installed version of mnco to knit and/or open:
+#'   load_all()/ctrl+shift+L
+#'   dailyReport()
+#'
+#' New data and updated reports still have to be committed and pushed using
+#'   terminal or rstudio gui in mcp-data
+#'
+#' @param knit Whether to knit
+#' @param open Whether to open
+#' @noRd
+dailyReport <- function(knit = TRUE, open = TRUE) {
+  if (knit) rmarkdown::render("../mcp-data/daily-report.Rmd")
+  if (open) system2("open", "../mcp-data/daily-report.pdf")
+}
