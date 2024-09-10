3da4e3c10edb84b2d14d59cf64b03090de47fe7f
tenergase <justinqdinh@yahoo.com>
2024-09-10 00:36:36 -0400

    Create dailyWorkflow() for downloading data, then automatically knitting, committing, and pushing to the mcp-data repo. It can be (and may be more convenient to) run from the mcno repo after load_all()/ctrl+shift+L.

--- R/qol.R
+++ R/qol.R
@@ -13,6 +13,15 @@ ccur <- function() getCenterData("curriculum")
 catt <- function() getCenterData("attendance")
 ctem <- function() getCenterData("template")
 
+cardRaffle <- function(width, seed, num = 3) {
+  cardWidth <- .1
+  numCards <- round(width / cardWidth)
+
+  set.seed(seed)
+  winners <- ceiling(runif(num, min = 0, max = numCards))
+
+  return(winners)
+}
 
 #' Knits daily-report.Rmd and opens it
 #'
@@ -41,56 +50,148 @@ ctem <- function() getCenterData("template")
 #'
 #' @export
 #' @noRd
-dailyReport <- function(knit = TRUE, open = TRUE) {
-  if (knit) rmarkdown::render("../mcp-data/daily-report.Rmd")
+dailyReport <- function(knit = TRUE, open = TRUE, push = FALSE) {
+  if (knit) {
+    knitTry <- try(rmarkdown::render("../mcp-data/daily-report.Rmd"))
+
+    knitError <- inherits(knitTry, "try-error")
+    if (knitError) {
+      stop(
+        "There was a problem knitting daily-report.Rmd. ",
+        "Please debug before knitting again using dailyReport().",
+        if (push) "\ndaily-report.pdf will not be committed and pushed automatically."
+      )
+    }
+
+    if (push) {
+      # Commit and push report
+      date <- format(Sys.Date(), format = "%m-%d-%y") %>%
+        stringr::str_replace_all("0", "")
+
+      git_cd("git add .")
+      git_cd("git commit -m \"", date, " report\"")
+      git_cd("git push")
+    }
+  }
+
   if (open) system2("open", "../mcp-data/daily-report.pdf")
 }
 
-cardRaffle <- function(width, seed, num = 3) {
-  cardWidth <- .1
-  numCards <- round(width / cardWidth)
+#' @export
+dailyWorkflow <- function(report = TRUE) {
+  # Make sure git config user.name and user.email are set in repo for rebase
+  #   and push to work
+  git_config()
 
-  set.seed(seed)
-  winners <- ceiling(runif(num, min = 0, max = numCards))
+  # Ensure working tree is clean (no uncommitted changes)
+  status <- git_cd("git status", intern = T)
+  if (!("nothing to commit, working tree clean" %in% status)) {
+    stop(
+      "You have uncommitted changes. ",
+      "Please commit or stash them before trying again:\n\t",
+      paste0(status, collapse = "\n\t")
+    )
+  }
 
-  return(winners)
+  # Fetch and pull with rebase
+  git_cd("git fetch")
+  rebase <- git_cd("git rebase origin/main main")
+
+  if (rebase != 0) {
+    git_cd("git rebase --abort")
+    stop(
+      "Conflicts arose when attemping to pull.\n",
+      "Please manually pull (with rebase) and resolve conflicts before trying again."
+    )
+  }
+
+  # Check if daily data is already downloaded
+  moveDataDownloads() %>% suppressMessages()
+  missing = inherits(try(getCenterData(), silent = T), "try-error")
+
+  if (!missing) {
+    message("The daily data is already downloaded. Justin will handle this case later. Please manually commit and push if needed.")
+    return(invisible(NULL))
+  }
+
+  # Open Radius for user to login
+  login = FALSE
+
+  shell.exec("https://radius.mathnasium.com/Account/Login?ReturnUrl=%2F")
+  while (!login) {
+    ans <- readline("Did you log in? (Y/N): ")
+    if(tolower(ans) != "y") {
+      message("Next time do the things and say \"Y\".")
+    } else {
+      login = TRUE
+    }
+  }
+
+  # Open Radius for user to download data
+  downloaded = FALSE
+
+  system2("open", getDataSources())
+  while (missing) {
+    ans <- readline("Did you download the files from the opened links? (Y/N): ")
+
+    moveDataDownloads()
+    missing = inherits(try(getCenterData(), silent = T), "try-error")
+
+    if(tolower(ans) != "y") {
+      message("Next time do the things and say \"Y\".")
+    } else if (missing) {
+      message("There are still missing files. Make sure they are downloaded. (Justin will figure out how to print which are missing)")
+    } else {
+      downloaded = TRUE
+    }
+  }
+  if (!downloaded) return(FALSE)
+
+  # Commit and push data
+  date <- format(Sys.Date(), format = "%m-%d-%y") %>%
+    stringr::str_replace_all("0", "")
+
+  git_cd("git add .")
+  git_cd("git commit -m \"", date, " data\"")
+  git_cd("git push")
+
+  # Knit and commit report
+  if (report) dailyReport(push = TRUE)
+
+  return(TRUE)
 }
 
-#' @export
-getDataSourceSites <- function(){
-  ret <- c("https://radius.mathnasium.com/Student",
-           "https://radius.mathnasium.com/CustomerAccount",
-           "https://radius.mathnasium.com/ProgressReportManager/CurrentBatchDetail",
-           "https://radius.mathnasium.com/AssessmentReport",
-           "https://radius.mathnasium.com/Enrollment/EnrollmentReport")
+getDataSources <- function() {
+  ret <- c(
+    "https://radius.mathnasium.com/Student",
+    "https://radius.mathnasium.com/CustomerAccount",
+    "https://radius.mathnasium.com/ProgressReportManager/CurrentBatchDetail",
+    "https://radius.mathnasium.com/Enrollment/EnrollmentReport",
+    "https://radius.mathnasium.com/AssessmentReport"
+  )
   return(ret)
 }
 
+git_cd <- function(..., path = "../mcp-data", intern = F) {
+  command <- paste(..., sep = "")
 
-openDataSources <- function(){
-  system2("open", getDataSourceSites())
+  stringr::str_replace(command, "git ", paste0("git -C ", path, " ")) %>%
+    shell(intern = intern) %>%
+    return()
 }
 
-#' @param push
-#'
-#' @export
-initialWorkflow <- function(push=FALSE){
+git_config <- function() {
+  config <- git_cd("git config --list", intern = T)
+  missing_name <- !any(stringr::str_detect(config, "^user\\.name"))
+  missing_email <- !any(stringr::str_detect(config, "^user\\.email"))
 
-  system2("open", "https://radius.mathnasium.com/Account/Login?ReturnUrl=%2F")
-  ans <- readline("Did you log in? (Y/N): ")
-  if(!stringr::str_detect(toupper(ans), "Y")){
-    stop("Next time do the things and say \"Y\".")}
-
-  openDataSources()
-  readline("Did you download the files from the opened links? (Y/N): ")
-  if(!stringr::str_detect(toupper(ans), "Y")){
-    stop("Next time do the things and say \"Y\".")}
-
-  mnco::dailyReport()
-  if(push){
-    shell(paste0("git add -A",
-                 # " && git commit -m \"",
-                 # Sys.Date(), " Initial Commit (Auto)\"",
-                 ""))
+  if (missing_name) {
+    name <- readline("git config user.name needs to be set locally for this repository. Enter your GitHub username: ")
+    git_cd("git config user.name ", name)
   }
+  if (missing_email) {
+    email <- readline("git config user.email needs to be set locally for this repository. Enter your GitHub email: ")
+    git_cd("git config user.email ", email)
+  }
+  invisible()
 }
