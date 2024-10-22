6404ff607b4c4f1487c86bdc6c73367fc14cadbf
tenergase <justinqdinh@yahoo.com>
2024-10-22 14:20:11 -0400

    [wip] more attendance dates and history work

--- R/attendance.R
+++ R/attendance.R
@@ -5,25 +5,39 @@ getAttendanceHistory <- function() {
   if (!file.exists(cachePath)) {
     saveAttendanceHistory(all = T)
   }
+
+  cache <- readRDS(cachePath)
+
+  return(cache)
 }
 
 #' @export
 saveAttendanceHistory <- function(all = F) {
-  if (all) saveAttendanceHistory.all
+  if (all)
+    saveAttendanceHistory.all()
 }
 
 saveAttendanceHistory.all <- function() {
-  history <- rawDataDir("Attendance Report 2022 to 10_8_2024.xlsx") %>%
-    {readRawData("path", path = .)} %>%
-    tidyRawData.attendance()
-
   attendanceDataDates <- list.files(rawDataDir()) %>%
     stringr::str_extract_all(
       "(?<=Student Attendance Report Export  )\\d{1,2}_\\d{1,2}_\\d{4}(?=\\.xlsx)"
     ) %>%
-    unlist()
+    unlist() %>%
+    as.Date(format = "%m_%d_%Y") %>%
+    sort()
 
-  for (date in attendanceDataDates) {
+  history <- rawDataDir("Attendance History 2022 to 10_21_2024.xlsx") %>%
+    {readRawData("path", path = .)} %>%
+    tidyRawData.attendance()
+
+  for (date in as.list(attendanceDataDates)) {
     data <- getCenterData("attendance", date = date)
+
+    history <- history %>%
+      dplyr::rows_upsert(data, by = c("Student", "Attendance_Date"))
   }
+
+  history <- history %>%
+    dplyr::arrange(.data$Student, .data$Attendance_Date) %>%
+    saveRDS(cacheDir("attendanceHistory.rds"))
 }
